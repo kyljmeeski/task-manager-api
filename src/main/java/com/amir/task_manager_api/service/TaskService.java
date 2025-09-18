0fed34395a0770ced9e0326402af080d01385d9b
kyljmeeski <kyljmeeski@gmail.com>
2025-09-18 10:37:36 +0600

    feat: add TaskService

---
+++ src/main/java/com/amir/task_manager_api/service/TaskService.java
@@ -0,0 +1,37 @@
+package com.amir.task_manager_api.service;
+
+import com.amir.task_manager_api.model.Task;
+import com.amir.task_manager_api.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
