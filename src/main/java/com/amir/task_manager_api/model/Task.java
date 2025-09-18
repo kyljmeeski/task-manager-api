aa4c05143d2e78bd5464e2ceee73303868fd7617
kyljmeeski <kyljmeeski@gmail.com>
2025-09-18 10:17:16 +0600

    feat: create Task entity with fields, timestamps and Lombok (#2)

---
+++ src/main/java/com/amir/task_manager_api/model/Task.java
@@ -0,0 +1,43 @@
+package com.amir.task_manager_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
+
+
