16b47fddb40bd34384c57364e8ab8be2a9c647c9
Amir Syrgabaev <a.syrgabaev@saimatelecom.kg>
2025-09-18 17:21:38 +0600

    feat: add all tasks caching

--- src/main/java/com/amir/task_manager_api/service/TaskService.java
+++ src/main/java/com/amir/task_manager_api/service/TaskService.java
@@ -1,9 +1,11 @@
 package com.amir.task_manager_api.service;
 
-import com.amir.task_manager_api.model.Task;
 import com.amir.task_manager_api.event.TaskCreatedEvent;
+import com.amir.task_manager_api.model.Task;
 import com.amir.task_manager_api.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +21,14 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final ApplicationEventPublisher publisher;
 
+    @CacheEvict(value = "tasks", allEntries = true)
     public Task createTask(Task task) {
         Task saved = taskRepository.save(task);
         publisher.publishEvent(new TaskCreatedEvent(this, saved));
         return saved;
     }
 
+    @Cacheable(value = "tasks")
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
@@ -33,10 +37,7 @@ public class TaskService {
         return taskRepository.findById(id);
     }
 
-    public Task updateTask(Task task) {
-        return taskRepository.save(task);
-    }
-
+    @CacheEvict(value = "tasks", allEntries = true)
     public Task takeToWork(long id) {
         Task task = taskRepository.findById(id).orElseThrow();
         if (task.getStatus() != NEW) {
@@ -46,6 +47,7 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
+    @CacheEvict(value = "tasks", allEntries = true)
     public Task resolve(long id) {
         Task task = taskRepository.findById(id).orElseThrow();
         if (task.getStatus() != IN_PROGRESS) {
@@ -55,6 +57,7 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
+    @CacheEvict(value = "tasks", allEntries = true)
     public Task cancel(long id) {
         Task task = taskRepository.findById(id).orElseThrow();
         if (task.getStatus() != IN_PROGRESS) {
@@ -64,6 +67,7 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
+    @CacheEvict(value = "tasks", allEntries = true)
     public void deleteTask(Long id) {
         Task task = taskRepository.findById(id).orElseThrow();
         taskRepository.deleteById(id);
